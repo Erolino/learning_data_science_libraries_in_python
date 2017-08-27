f7591aa0dae2502920a81885f24e21ca8f613f73
Zafrin Minoti <zafrin.minoti@gmail.com>
2017-08-27 02:13:37 -0400

    filters/interections added. app.callback with 1 function added to dash app

--- Dash/dash_real_app.py
+++ Dash/dash_real_app.py
@@ -5,12 +5,12 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
-#from plotly.offline import download_plotlyjs, plot, iplot
-#import cufflinks as cf
-#cf.go_offline()
+ny_daily = pd.read_pickle('ny_daily')
+print(ny_daily.head())
 
-df = pd.read_pickle('ny_daily')
-print(df.head())
+sales_by_city = pd.read_pickle('sales_by_city')
+print(sales_by_city.head())
+routes = sales_by_city['Route Name'].unique()
 
 app = dash.Dash()
 
@@ -19,33 +19,87 @@ first_p = '''Dash: A web application framework for Python.'''
 
 traces = []
 
-for day in df['Day of Week'].unique():
-    filtered_df = df[df['Day of Week']==day]
+for day in ny_daily['Day of Week'].unique():
+    filtered_ny_daily = ny_daily[ny_daily['Day of Week']==day]
     traces.append(go.Scatter(
-        x = filtered_df['Travel Date'],
-        y = filtered_df['Net Amount'],
+        x = filtered_ny_daily['Travel Date'],
+        y = filtered_ny_daily['Net Amount'],
         mode = 'lines',
         name = day
         ))
 
-print(traces)
+#print(traces)
 
 app.layout = html.Div(children=[
     html.H1(children=h1),
 
     html.Div(children=first_p),
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': traces,
-            'layout': {
-                'title' : 'NY to DC Revenue by Day of the Week'
+    html.Div([
+        dcc.Dropdown(
+            id = 'route_selection',
+            options = [{'label': route, 'value': route} for route in routes],
+            multi = True
+        )
+    ],
+    style={'width': '48%', 'display': 'inline-block'}),
 
+    html.Div([
+        dcc.Dropdown(
+            id = 'yaxis_catagory',
+            options = [{'label': item, 'value': item} for item in ['Adults','Net Amount','Occupancy']],
+            value = 'Net Amount'
+        )
+    ],
+    style={'width': '48%', 'float': 'right' ,'display': 'inline-block'}),
+
+    html.Div([
+        dcc.Graph(
+            id='stops',
+            figure={
+                'data': traces,
+                'layout': {
+                    'title' : 'NY to DC Revenue by Day of the Week'
+
+                }
             }
-        }
-    )
+        )
+    ], style={'width': '100%', 'display': 'inline-block', 'padding': '0 20'}),
+
+    html.Div([
+        dcc.Graph(
+            id='weekly_growth_by_day',
+
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'})
+
 ])
+
+
+@app.callback(
+    dash.dependencies.Output('weekly_growth_by_day','figure'),
+    [dash.dependencies.Input('yaxis_catagory','value')])
+
+def update_weekly_growth_by_day(y_axis_catagory_name):
+    df = ny_daily
+
+    traces = []
+
+    for day in ny_daily['Day of Week'].unique():
+        filtered_ny_daily = ny_daily[ny_daily['Day of Week']==day]
+        traces.append(go.Scatter(
+            x = filtered_ny_daily['Travel Date'],
+            y = filtered_ny_daily[y_axis_catagory_name],
+            mode = 'lines',
+            name = day
+            ))
+
+    return {
+        'data': traces,
+        'layout': {
+            'title' : 'NY to DC Revenue by Day of the Week'
+        }
+    }
 
 
 if __name__ == '__main__':
